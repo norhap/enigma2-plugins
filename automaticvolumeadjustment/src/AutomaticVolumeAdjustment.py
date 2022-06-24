3ecb2b4b602312d7a5c2b525f9938de082013ff1
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2022-06-24 21:58:15 +0200

    automaticvolumeadjustment: Use print() function

--- automaticvolumeadjustment/src/AutomaticVolumeAdjustment.py
+++ automaticvolumeadjustment/src/AutomaticVolumeAdjustment.py
@@ -152,7 +152,7 @@ class AutomaticVolumeAdjustment(Screen):
 				try:
 					self.volumeControlInstance = VolumeControl.instance
 				except:
-					print "[AutomaticVolumeAdjustment] VolumeControl.instance is None"
+					print("[AutomaticVolumeAdjustment] VolumeControl.instance is None")
 				self.pluginStarted = True # plugin started...
 
 	def isCurrentAudioAC3DTS(self):
