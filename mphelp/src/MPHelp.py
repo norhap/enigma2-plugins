2963e2d2293c84391d347f577655e4d87f5c9c77
norhap <elfogondesegovia@gmail.com>
2022-06-27 23:11:33 +0200

    MPHelp.py: fix remove prefix "b" and final "'" for text XML
    prefix byte object is not a Unicode str object

--- mphelp/src/MPHelp.py
+++ mphelp/src/MPHelp.py
@@ -84,7 +84,7 @@ class MPHelp(Screen):
 			text = "You managed to jump to an invalid page. Stop it :-)"
 			newPage = self.curPage
 		self["title"].text = title.encode('utf-8', 'ignore')
-		self["detailtext"].setText(text.encode('utf-8', 'ignore'))
+		self["detailtext"].setText(str(text))
 		self.curPage = newPage
 
 	def pageUp(self):
