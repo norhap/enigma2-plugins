c62c5d44b0f62128ab3a1e7743f2120c02641da9
norhap <elfogondesegovia@gmail.com>
2023-01-01 02:35:15 +0100

    Remove Screen Rc use ShowRemoteControl from HelpMenu
    follow https://github.com/norhap/enigma2-openvision-1/commit/75d7c23bd9b93a481c7d1200dc7360c04f30aa10

--- autotimer/src/AutoTimerWizard.py
+++ autotimer/src/AutoTimerWizard.py
@@ -3,7 +3,7 @@ from . import _
 
 # GUI (Screens)
 from Screens.WizardLanguage import WizardLanguage
-from Screens.Rc import Rc
+from Screens.HelpMenu import ShowRemoteControl
 from .AutoTimerEditor import AutoTimerEditorBase, AutoTimerServiceEditor, \
 		AutoTimerFilterEditor
 
@@ -22,7 +22,7 @@ from Tools import Directories
 from .Logger import doLog
 
 
-class AutoTimerWizard(WizardLanguage, AutoTimerEditorBase, Rc):
+class AutoTimerWizard(WizardLanguage, AutoTimerEditorBase, ShowRemoteControl):
 	STEP_ID_BASIC = 2
 	STEP_ID_TIMESPAN = 5
 	STEP_ID_SERVICES = 7
@@ -55,7 +55,7 @@ class AutoTimerWizard(WizardLanguage, AutoTimerEditorBase, Rc):
 
 		WizardLanguage.__init__(self, session, showSteps=True, showStepSlider=True)
 		AutoTimerEditorBase.__init__(self, newTimer)
-		Rc.__init__(self)
+		ShowRemoteControl.__init__(self)
 
 		#self.skinName = ["AutoTimerWizard", "NetworkWizard"]
 		self["wizard"] = Pixmap()
