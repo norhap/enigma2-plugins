d1a235b4a639bd0761a5737ac265fb6798557c1e
norhap <elfogondesegovia@gmail.com>
2025-11-12 21:40:13 +0100

    [imdb] Fix a search that didn't find anything
    Co-Authored-By: Jason Hood <jadoxa@yahoo.com.au>

--- imdb/src/plugin.py
+++ imdb/src/plugin.py
@@ -1001,10 +1001,9 @@ class IMDB(Screen, HelpableScreen):
 			self["detailslabel"].setText(_("IMDb query failed!"))
 
 	def searchPlot(self):
-		try:
-			self["statusbar"].setText(self["menu"].getCurrent()[2])
-		except Exception:
-			self["statusbar"].setText("")
+		cur = self["menu"].getCurrent()
+		if cur:
+			self["statusbar"].setText(cur[2])
 
 	def http_failed(self, failure):
 		text = _("IMDb Download failed")
